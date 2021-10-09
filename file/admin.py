82ce9f7f94ad397e866a61e2826b3b864c4932c5
Mikhail <mikhail349349@gmail.com>
2021-10-09 18:11:10 +0300

    upd

--- file/admin.py
+++ file/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 
 from .models import File, AssetFile
 
-admin.site.register(File)
-admin.site.register(AssetFile)
+class FileAdmin(admin.ModelAdmin):
+      exclude = ('name',)
+
+class AssetFileAdmin(FileAdmin):
+    pass
+
+#admin.site.register(File, FileAdmin)
+admin.site.register(AssetFile, AssetFileAdmin)
--- file/models.py
+++ file/models.py
@@ -7,25 +7,23 @@ import os
 
 from asset.models import Asset
 
-
 def get_file_path(instance, filename):
     ext = filename.split('.')[-1]
     filename = "%s.%s" % (uuid.uuid4(), ext)
     return filename
 
 class File(models.Model):
-    filename = models.CharField(max_length=255, null=True, blank=True)
-    data = models.FileField(upload_to=get_file_path)
+    name = models.CharField(max_length=255, null=True, blank=True)
+    file = models.FileField(upload_to=get_file_path)
 
     def __str__(self):
-        return self.filename
+        return self.name or ''
 
-class AssetFile(models.Model):
-    file = models.OneToOneField(File, on_delete=models.CASCADE, primary_key=True)
+class AssetFile(File):
     asset = models.ForeignKey(Asset, on_delete=models.CASCADE, related_name='files')
-
+    
     def __str__(self):
-        return '%s - %s' % (self.file, self.asset)
+        return '%s - %s' % (self.name, self.asset)
 
 def _delete_file(path):
     try:
@@ -33,18 +31,18 @@ def _delete_file(path):
     except:
         pass
 
-@receiver(models.signals.pre_save, sender=File)
+@receiver(models.signals.pre_save, sender=AssetFile)
 def pre_save_file(sender, instance, *args, **kwargs):
     if not instance.pk:
-        instance.filename = instance.data.name
+        instance.name = instance.file.name
     else:
-        cur_file_data = File.objects.get(pk=instance.pk).data
-        if cur_file_data != instance.data:
-            instance.filename = instance.data.name
-            _delete_file(cur_file_data.path)
+        cur_file = AssetFile.objects.get(pk=instance.pk).file
+        if cur_file != instance.file:
+            instance.name = instance.file.name
+            _delete_file(cur_file.path)
 
-@receiver(models.signals.post_delete, sender=File)
+@receiver(models.signals.post_delete, sender=AssetFile)
 def post_delete_file(sender, instance, *args, **kwargs):
-    if instance.data:
-        _delete_file(instance.data.path)
+    if instance.file:
+        _delete_file(instance.file.path)
 
