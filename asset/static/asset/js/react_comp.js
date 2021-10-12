52fe40fd0f23c95a74aebb30ecdaaf3c46be2681
Mikhail <mikhail349349@gmail.com>
2021-10-12 23:08:56 +0300

    create_update_html - add files

--- asset/static/asset/js/react_comp.js
+++ asset/static/asset/js/react_comp.js
@@ -1,16 +1,44 @@
 function LikeButton() {
-    const [liked, setLiked] = React.useState(false);
-
-    if (liked) {
-        return 'You liked this.';
-    }
+    const [files, setFiles] = React.useState([]);
+    const fileInputRef = React.createRef();
     
+    function onClick(e) {
+        fileInputRef.current.click();
+        //let newFiles = [...files];
+        //newFiles.unshift("test");
+        //setFiles(newFiles)
+    
+    }
+
+    function onFileInputChange(e) {
+        setFiles([...files, ...e.target.files]);
+        fileInputRef.current.value = null;
+    }
+
+    console.log(files);
+
     return (
-        <button onClick={() => setLiked(true)}>
-            Like!
-        </button>        
+        <div className="row">
+            <input ref={fileInputRef} type="file" multiple hidden onChange={onFileInputChange} />
+            {
+                files.map((file, index) => (
+                    <div key={index} className="col-md-4">
+                        <div className="card box-shadow text-center h-100" /*style={{width: "100px"}}*/>
+                            <img src={URL.createObjectURL(file)} class="card-img" alt="..." />   
+                        </div>
+                    </div>                    
+                ))
+            }
+            <div className="col-md-4">
+                <div className="card box-shadow text-center h-100" /*style={{width: "100px"}}*/>
+                    <div className="card-body align-items-center d-flex justify-content-center">
+                        <div class="btn btn-primary" onClick={onClick}>Добавить фото</div>
+                    </div>    
+                </div>
+            </div>
+        </div>     
     )
 }
 
-const domContainer = document.querySelector('#app');
+const domContainer = document.querySelector('#react_app');
 ReactDOM.render(<LikeButton />, domContainer);
