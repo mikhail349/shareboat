8b5f0a00752292fa9ee1c8ad512eadd804f42cf9
Mikhail <mikhail349349@gmail.com>
2021-10-24 22:18:12 +0300

    added hash static

--- shareboat/settings.py
+++ shareboat/settings.py
@@ -163,6 +163,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 LOGIN_URL = '/user/login/'
 
 
