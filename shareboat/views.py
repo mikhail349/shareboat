f68d79254dfa85c7c145d08951e90883c8fe40bf
Mikhail <mikhail349349@gmail.com>
2021-11-04 16:06:02 +0300

    Update views.py

--- shareboat/views.py
+++ shareboat/views.py
@@ -1,10 +1,6 @@
 from django.shortcuts import render
 
-import logging
-logger = logging.getLogger(__name__)
-
 def index(request):
-    logger.error("test file console")
     return render(request, 'home.html')
 
 def not_found(request):
