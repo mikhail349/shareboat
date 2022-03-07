ed7f67ffe6dacdd0a4475923c85526da2d45296e
Mikhail <mikhail349349@gmail.com>
2022-03-07 13:12:09 +0300

    upd

--- static/js/nav.js
+++ static/js/nav.js
@@ -1,18 +1,30 @@
-var lastScrollTop = 0;
+var lastScrollTop = 0,
+     currentNavTop = 0,
+     currentToastTop = 62;     
 
 /*$(document).ready(() => {
      $('.navbar-nav a.active').removeClass('active bg-light text-dark');
      $('.navbar-nav a[href="' + location.pathname + '"').addClass("active bg-light text-dark");
 })*/
 
-window.addEventListener("scroll", () => {
-   var st = window.pageYOffset || document.documentElement.scrollTop;
-   if (st > lastScrollTop && st > 62){
-        $('body > header > nav').css("top", "-80px");
-        $('.toast-container ').attr("style", "top: 0px !important;");
-   } else {
-        $('body > header > nav').css("top", "0");
-        $('.toast-container ').attr("style", "top: 62px !important;");
-   }
-   lastScrollTop = st <= 0 ? 0 : st;
-}, false);
+$(document).ready(() => {
+
+     const $mainNav = $('body > header > nav');
+     const $mainToast = $('#toastContainer');
+
+     const mainNavHeight = $mainNav.outerHeight();
+
+     function mainMenuHide(){
+          const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+          const diffScrollTop = scrollTop - lastScrollTop;
+          currentNavTop += diffScrollTop;
+          currentNavTop = currentNavTop > mainNavHeight ? mainNavHeight : currentNavTop < 0 ? 0 : currentNavTop;
+
+          $mainNav.attr("style", `top: -${currentNavTop}px !important;`);
+          $mainToast.attr("style", `top: ${-currentNavTop+mainNavHeight}px !important;`);
+          
+          lastScrollTop = scrollTop;
+     }
+
+     window.addEventListener("scroll", mainMenuHide, false);
+});
