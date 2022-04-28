226460afc715e1774473058d8460f37b2a8bfe45
Mikhail <mikhail349349@gmail.com>
2022-04-28 21:30:09 +0300

    Update send_test_email.py

--- scheduler/send_test_email.py
+++ scheduler/send_test_email.py
@@ -5,11 +5,15 @@ import argparse
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--project_path", type=str)
+parser.add_argument("--project_path", required=False, type=str)
 parser.add_argument("--recipients", type=str)
 args = parser.parse_args()
 
-sys.path.append(args.project_path)
+if args.project_path:
+    sys.path.append(args.project_path)
+else:
+    sys.path.append('../')
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shareboat.settings')
 django.setup()
 
