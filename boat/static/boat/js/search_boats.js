291dc8ff8fc0c8d31a257e4754149a884da943fa
Mikhail <mikhail349349@gmail.com>
2022-05-27 20:00:55 +0300

    upd

--- boat/static/boat/js/search_boats.js
+++ boat/static/boat/js/search_boats.js
@@ -1,7 +1,19 @@
 $(document).ready(() => {
     $('form').on('submit', function(e) {
-        const $btnSubmit = $('form button[type=submit]');
-        $btnSubmit.attr('disabled', true);
-        $btnSubmit.text('Идет поиск...');
+
+        var isInvalid = false;
+        for (let input of $("form input")) {
+            if ($(input).is(':invalid')) {
+                isInvalid = true;
+                break;
+            }
+        }
+        
+        if (!isInvalid) {
+            const $btnSubmit = $('form button[type=submit]');
+            $btnSubmit.attr('disabled', true);
+            $btnSubmit.text('Идет поиск...');
+        }
+
     });
 });
