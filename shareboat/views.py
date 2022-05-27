efa14cc54c7342eb7b64360c2152f8d4306e04ef
mikhail349 <mikhail349349@gmail.com>
2022-05-27 15:53:53 +0300

    Update views.py

--- shareboat/views.py
+++ shareboat/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.contrib.staticfiles.storage import staticfiles_storage
 
 def sw(request):
-    file = staticfiles_storage.path('/sw.js')
+    file = staticfiles_storage.path('js/sw.js')
     return HttpResponse(open(file).read(), content_type="application/javascript")
 
 def index(request):
