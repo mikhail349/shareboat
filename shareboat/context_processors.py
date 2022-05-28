0cbb1e6f02512f2771abc40b29eb8059162d7fc3
Mikhail <mikhail349349@gmail.com>
2022-05-28 18:15:31 +0300

    added notification badges

--- shareboat/context_processors.py
+++ shareboat/context_processors.py
@@ -1,10 +1,14 @@
 from boat.models import Boat
+from booking.models import Booking
 
 def nav_counters(request):
     boats_on_moderation_count = Boat.objects.filter(status=Boat.Status.ON_MODERATION).count()
+    bookings_on_pending_count = Booking.objects.filter(boat__owner=request.user, status=Booking.Status.PENDING).count()
     return {
         'nav_counters': {
-            'boats_on_moderation_count': boats_on_moderation_count
+            'boats_on_moderation_count': boats_on_moderation_count,
+            'bookings_on_pending_count': bookings_on_pending_count,
+            'total': boats_on_moderation_count + bookings_on_pending_count
         }
     } 
     
