faa024ddbfd679ecb73163fea400211c6329a555
Mikhail <mikhail349349@gmail.com>
2022-05-29 15:51:57 +0300

    bug fix

--- shareboat/context_processors.py
+++ shareboat/context_processors.py
@@ -3,7 +3,9 @@ from booking.models import Booking
 
 def nav_counters(request):
     boats_on_moderation_count = Boat.objects.filter(status=Boat.Status.ON_MODERATION).count()
-    bookings_on_pending_count = Booking.objects.filter(boat__owner=request.user, status=Booking.Status.PENDING).count()
+    bookings_on_pending_count = 0
+    if request.user.is_authenticated:
+        bookings_on_pending_count = Booking.objects.filter(boat__owner=request.user, status=Booking.Status.PENDING).count()
     return {
         'nav_counters': {
             'boats_on_moderation_count': boats_on_moderation_count,
