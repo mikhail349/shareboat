09e2e074e8347034055dc1c5c48c24cec697797d
mikhail349 <mikhail349349@gmail.com>
2022-09-02 15:34:45 +0300

    upd

--- boat/static/boat/js/form-files.js
+++ boat/static/boat/js/form-files.js
@@ -3,6 +3,7 @@ function FilesList(props) {
     const [files, setFiles] = React.useState([]);
     const [count, setCount] = React.useState(0);
     const fileInputRef = React.createRef();
+    const imgStyle = {height: '150px'};
 
     React.useEffect(() => {
         if (props.boatId) {
@@ -76,59 +77,59 @@ function FilesList(props) {
     }
 
     return (
-        <div className={`file-upload-wrapper ${isFileUploadHover() ? "bg-light" : ""}`} 
-            onDragEnter={onDragEnter}
-            onDragLeave={onDragLeave}
-            onDrop={onDrop}
-            onDragOver={(e) => e.preventDefault()}
-        >    
+        <div>  
             <input ref={fileInputRef} type="file" name="hidden_files" accept="image/*" multiple hidden onChange={onFileInputChange} />
-            {
-                files.length > 0 && (
-                    <div className="row g-3 mb-3">      
-                        {
-                            files.map((file, index) => (
-                                <div key={index} className="col-6 col-md-4 col-lg-3">
-                                    <div className="card box-shadow text-end">
-                                        <img src={file.url} className="card-img" style={{height: '150px'}} data-filename={file.blob.name} />  
-                                        <div className="card-img-overlay">
-                                            <div className="btn-group">
-                                                <button type="button" className="btn btn-sm btn-danger" onClick={() => deleteFile(index)}>
-                                                    Удалить
-                                                </button>
-                                            </div>
-                                        </div>
+            <div className="row g-3 mb-3">      
+                {
+                    files.map((file, index) => (
+                        <div key={index} className="col-md-4 col-lg-3">
+                            <div className="card box-shadow text-end">
+                                <img src={file.url} className="card-img" style={imgStyle} data-filename={file.blob.name} />  
+                                <div className="card-img-overlay">
+                                    <div className="btn-group">
+                                        <button type="button" className="btn btn-sm btn-danger" onClick={() => deleteFile(index)}>
+                                            Удалить
+                                        </button>
                                     </div>
-                                </div>                    
-                            ))
-                        }                
-                    </div>
-                )
-            }
-            
+                                </div>
+                            </div>
+                        </div>                    
+                    ))
+                }
+                {
+                    !window.isMobile() && (
+                        <div className="col-md-4 col-lg-3">
+                            <div className={`card shadow-none text-end border border-dashed photo-upload-wrapper ${isFileUploadHover() ? "photo-upload-wrapper-hover" : ""}`}
+                                onDragEnter={onDragEnter}
+                                onDragLeave={onDragLeave}
+                                onDrop={onDrop}
+                                onDragOver={(e) => e.preventDefault()}
+                            >
+                                <div className="card-img d-flex justify-content-center align-items-center text-center" style={imgStyle}>
+                                    {
+                                        isFileUploadHover() ? (
+                                            <span className="text-primary">
+                                                Отпустите, чтобы добавить фото
+                                            </span>  
+                                        ) : (
+                                            <span className="text-muted">
+                                                Перетащите фото сюда или нажмите "Добавить фото"
+                                            </span>     
+                                        )
+                                    }
+                                </div>  
+                            </div>
+                        </div>   
+                    )
+                } 
+                
+            </div>
             <div class="row row-cols-lg-auto align-items-center">
                 <div class="col">
                     <button type="button" className="btn btn-outline-primary w-100" onClick={() => fileInputRef.current.click()}>
                         Добавить фото
                     </button>  
-                </div> 
-                {
-                    !window.isMobile() && (
-                        <div class="col">
-                        {
-                            isFileUploadHover() ? (
-                                <span className="text-success">
-                                    Отпустите, чтобы добавить фото
-                                </span>  
-                            ) : (
-                                <span className="text-muted">
-                                    Или перетащите фото сюда
-                                </span>     
-                            )
-                        }
-                    </div>  
-                    )
-                }
+                </div>
             </div>
         </div>
     )
