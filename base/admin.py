b8c28ce76cc278c15acc0708298e98b2251f51b1
Mikhail <mikhail349349@yandex.ru>
2022-09-07 16:46:48 +0300

    autopep8 added

--- base/admin.py
+++ base/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from . import models
 
+
 class BaseImageInline(admin.TabularInline):
     model = models.BaseImage
 
+
 class BaseAdmin(admin.ModelAdmin):
     inlines = [BaseImageInline]
+
 
 admin.site.register(models.Base, BaseAdmin)
--- base/models.py
+++ base/models.py
@@ -1,22 +1,23 @@
 from django.db import models
 from django.core.validators import RegexValidator
 from django.utils.translation import gettext_lazy as _
-from django.db.models.signals import pre_save, post_save, post_delete
+from django.db.models.signals import pre_save, post_save
 
 from file import utils, signals
 
+
 class Base(models.Model):
-    name    = models.CharField(max_length=255)
-    lon     = models.DecimalField(max_digits=9, decimal_places=6)
-    lat     = models.DecimalField(max_digits=9, decimal_places=6)
+    name = models.CharField(max_length=255)
+    lon = models.DecimalField(max_digits=9, decimal_places=6)
+    lat = models.DecimalField(max_digits=9, decimal_places=6)
     address = models.TextField()
-    state   = models.CharField(max_length=255, db_index=True)
-    
+    state = models.CharField(max_length=255, db_index=True)
+
     website = models.URLField(max_length=255, null=True, blank=True)
-    phone   = models.CharField(
-        max_length=10, 
-        null=True, 
-        blank=True, 
+    phone = models.CharField(
+        max_length=10,
+        null=True,
+        blank=True,
         validators=[
             RegexValidator(
                 regex=r'(\d+){10}',
@@ -29,9 +30,13 @@ class Base(models.Model):
     def __str__(self):
         return f'{self.name} ({self.state})'
 
+
 class BaseImage(models.Model):
     image = models.ImageField(upload_to=utils.get_file_path)
-    base = models.ForeignKey(Base, on_delete=models.CASCADE, related_name='images')
+    base = models.ForeignKey(Base,
+                             on_delete=models.CASCADE,
+                             related_name='images')
+
 
 pre_save.connect(signals.verify_imagefile, sender=BaseImage)
 post_save.connect(signals.compress_imagefile, sender=BaseImage)
