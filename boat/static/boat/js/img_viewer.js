a1a79473e561cf721382b508d7710a79a72027ba
Mikhail <mikhail349349@gmail.com>
2022-09-09 16:23:18 +0300

    card-img-small opening fixed

--- boat/static/boat/js/img_viewer.js
+++ boat/static/boat/js/img_viewer.js
@@ -1,7 +1,7 @@
 $(document).ready(() => {
     $(".card-img-small").on('click', function(e) {
         const src = $(this).attr('src');
-        $('.card-img-md').attr('src', src);
+        $('.card-img-main').attr('src', src);
 
         $(".card-img-small").removeClass('active');
         $(this).addClass('active');
